c21b4b72d4cc1f3e92a03b6f2e1590bbc938c68e
Mateo1O <compla1@hotmail.com>
2023-07-03 21:35:33 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+import models
 
 
 class BaseModel:
@@ -8,6 +9,7 @@ class BaseModel:
     def __init__(self, *args, **kw):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         pass
