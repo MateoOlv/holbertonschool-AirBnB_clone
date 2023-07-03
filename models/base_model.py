6fabf06b37a760cb26eb7169e3d88f3d4101608e
jeremias <jeremiaserba7894@gmail.com>
2023-07-03 20:46:05 +0000

    Building structure of project

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+
+class BaseModel:
+    """ A class representing a BaseModel """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        pass
+
+    def save(self):
+        pass
+
+    def to_dict(self):
