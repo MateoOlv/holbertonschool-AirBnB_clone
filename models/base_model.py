f7a7eeabde254e0880fc93d2f93ef69ce9efc43e
jeremias <jeremiaserba7894@gmail.com>
2023-07-03 21:46:19 +0000

    Development of to_dict function in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@ import uuid
 import datetime
 
 
-
 class BaseModel:
     """ A class representing a BaseModel """
     def __init__(self):
@@ -20,4 +19,10 @@ class BaseModel:
         self.update_at = datetime.now()
 
     def to_dict(self):
-        pass
+        """ Return a dictionary representation of a instance"""
+
+        new_dict = self.__dict__.copy()
+        new_dict['__name__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['update_at'] = self.update_at.isoformast()
+        return new_dict
