d3ef1feb50279475d65cb81a75db8e03eff247a7
jeremias <jeremiaserba7894@gmail.com>
2023-07-04 18:24:37 +0000

    Task 4 - First commit

--- models/base_model.py
+++ models/base_model.py
@@ -32,11 +32,21 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init Value"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if ((key != "__class__") and
+                        key == "created_at" or key == "updated_at"):
+                    setattr(self, key, datetime.strptime(
+                        value,
+                        "%Y-%m-%dT%H:%M:%S.%f",
+                    ))
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Str return"""
