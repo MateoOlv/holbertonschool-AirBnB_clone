7ccc484223610dfa044ff666b36900542f0d0b5a
jeremias <jeremiaserba7894@gmail.com>
2023-07-04 14:23:41 +0000

    Add features 1 - Use of kwargs for handle cases in which the user creates an instance with key-value and set these to attributes. 2 - Add comments in __init__.py.

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-"""Imports"""
+"""
+Imports
+"""
 import uuid
 from datetime import datetime
 
 
+"""BaseModel Module"""
+
+
 class BaseModel:
-    """ A class representing a BaseModel """
-    def __init__(self):
-        """init values"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Init Value"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(
+                            value,
+                            "%Y-%m-%dT%H:%M:%S.%f",
+                        ))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """ return [<class name>] (<self.id>) <self.__dict__> """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Str return"""
+        datad = self.__class__.__name__
+        return "[{}] ({}) {}".format(datad, self.id, self.__dict__)
 
     def save(self):
-        """ Update date of update_at """
+        """Save"""
         self.update_at = datetime.now()
 
     def to_dict(self):
-        """ Return a dictionary representation of a instance"""
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = str(self.created_at.isoformat())
-        dict_copy['update_at'] = str(self.update_at.isoformat())
-        return dict_copy
+        """Dict values"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
