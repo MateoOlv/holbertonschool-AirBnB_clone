4c22f58d5b3fa1ff59b8584c350dad43b6967dfa
MateoOlv <6304@holbertonstudents.com>
2023-07-04 06:25:47 -0700

    commentaries

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Imports"""
+"""
+Imports
+"""
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
-    """ A class representing a BaseModel """
+"""BaseModel Module"""
 
+
+class BaseModel:
     def __init__(self):
-        """init values"""
+        """Init Value"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """ return [<class name>] (<self.id>) <self.__dict__> """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Str return"""
+        datad = self.__class__.__name__
+        return "[{}] ({}) {}".format(datad, self.id, self.__dict__)
 
     def save(self):
-        """ Update date of update_at """
+        """Save"""
         self.update_at = datetime.now()
 
     def to_dict(self):
-        """ Return a dictionary representation of a instance"""
+        """Dict values"""
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
