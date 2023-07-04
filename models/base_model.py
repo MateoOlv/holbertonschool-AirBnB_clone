02a913c9a5d2b97779566a19f4e211e2bc06f1b2
jeremias <jeremiaserba7894@gmail.com>
2023-07-04 18:28:10 +0000

    Task 4 Fix in order of if-else sentence.

--- models/base_model.py
+++ models/base_model.py
@@ -36,17 +36,17 @@ class BaseModel:
         """Init Value"""
         if kwargs is not None:
             for key, value in kwargs.items():
-                if ((key != "__class__") and
-                        key == "created_at" or key == "updated_at"):
-                    setattr(self, key, datetime.strptime(
-                        value,
-                        "%Y-%m-%dT%H:%M:%S.%f",
-                    ))
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(
+                            value,
+                            "%Y-%m-%dT%H:%M:%S.%f",
+                        ))
+                    else:
+                        setattr(self, key, value)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Str return"""
