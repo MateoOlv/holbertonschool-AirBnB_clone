4ee5ce825d5e5c640a3acf7524a5777ada722429
MateoOlv <6304@holbertonstudents.com>
2023-07-05 06:55:07 -0700

    File storage base code

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
-#!/usr/bin/python3
-""" Initialize the main. """
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import os.path
+import json
+from models.base_model import BaseModel
+"""
+Import
+"""
+
+
+class FileStorage:
+    """"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        all
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        new
+        """
+        name = obj.__class__.__name__
+        FileStorage.__objects[name + '.' + obj.id] = obj
+
+    def save(self):
+        """
+        save
+        """
+        jData = {}
+        for key, value in FileStorage.__objects.items():
+            jData[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            data = json.dump(jData, f)
+
+    def reload(self):
+        """
+        Reload
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as jsonfile:
+                jData = json.load(jsonfile)
+                for key, obj in jData.items():
+                    self.new(setattr(self, 'new', eval(
+                        obj['__class__'])(**obj)))
+        except:
+            return
