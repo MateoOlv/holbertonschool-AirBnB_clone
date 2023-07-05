f51660323e4a6f59ad6cf85fe7a1391425580d31
MateoOlv <6304@holbertonstudents.com>
2023-07-05 09:43:32 -0700

    typo Error

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Imports
 """
 import uuid
 from datetime import datetime
+import models
 
 
 """
@@ -34,19 +35,14 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Init Value"""
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(
-                            value,
-                            "%Y-%m-%dT%H:%M:%S.%f",
-                        ))
-                    else:
-                        setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        models.storage.net(self)
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Str return"""
@@ -55,11 +51,13 @@ class BaseModel:
 
     def save(self):
         """Save"""
+        models.storage.save()
         self.update_at = datetime.now()
 
     def to_dict(self):
         """Dict values"""
         data = self.__dict__.copy()
+
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
