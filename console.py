51a263d8b395366a8fb08ff247d13682e74a0d4d
jeremias <jeremiaserba7894@gmail.com>
2023-07-06 15:57:07 +0000

    Task 7 - Add do_create method

--- console.py
+++ console.py
@@ -6,11 +6,26 @@ Imports
 
 """
 import cmd
+import json
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+
+""" Classes """
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ HBNB command """
     prompt = '(hbnb) '
+    classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Amenity",
+        "Place",
+        "Review"
+    }
 
     def do_quit(self, arg):
         """
@@ -33,6 +48,22 @@ class HBNBCommand(cmd.Cmd):
             this function is called.
         """
         pass
+
+    def do_create(self, arg):
+        """
+            to_create()
+            Creates a new instance of BaseModel, saves it (to the JSON file).
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            objClassName = globals().get(arg)
+            instance = objClassName()
+            storage.new(instance)
+            storage.save()
+            print(instance.id)
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        models.storage.net(self)
+        models.storage.new(self)
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
