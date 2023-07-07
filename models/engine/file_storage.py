7edfa72893276995ac108cffe4d79de075118538
jeremias <jeremiaserba7894@gmail.com>
2023-07-07 15:56:29 +0000

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,19 +3,23 @@ import os.path
 from models.base_model import BaseModel
 from models.user import User
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ returns the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
         if obj is not None and hasattr(obj, 'id'):
-            name = obj.__class__.__name__
-            FileStorage.__objects[name + '.' + obj.id] = obj
+            name = f"{obj.__class__.__name__}.{obj.id}"
+            FileStorage.__objects[name] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
         jData = {}
         for key, value in FileStorage.__objects.items():
             jData[key] = value.to_dict()
@@ -23,12 +27,10 @@ class FileStorage:
             json.dump(jData, f)
 
     def reload(self):
-        try:
-            if os.path.exists(FileStorage.__file_path):
-                with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                    data = json.load(f)
-                    for key, obj in data.items():
-                        newObj = eval(obj['__class__'])(**obj)
-                        FileStorage.__objects[key] = newObj
-        except FileNotFoundError:
-            pass
+        """ deserializes the JSON file to __objects """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                for key, obj in data.items():
+                    newObj = eval(obj['__class__'])(**obj)
+                    FileStorage.__objects[key] = newObj
