40a43ae80872c2a2e8b63c5b803c1c3cecf00b7f
jeremias <jeremiaserba7894@gmail.com>
2023-07-07 15:40:47 +0000

    Task 5. - Add aditional verification in new() method. - Add additional verification in reload() method, if path exists.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,50 +1,34 @@
-#!/usr/bin/python3
-import os
 import json
+import os.path
 from models.base_model import BaseModel
 from models.user import User
-"""Import
-"""
-
 
 class FileStorage:
-    """"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        all
-        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        new
-        """
-        if obj is not None:
+        if obj is not None and hasattr(obj, 'id'):
             name = obj.__class__.__name__
             FileStorage.__objects[name + '.' + obj.id] = obj
 
     def save(self):
-        """
-        save
-        """
         jData = {}
         for key, value in FileStorage.__objects.items():
             jData[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(jData, f)
 
     def reload(self):
-        """
-        Reload
-        """
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, obj in data.items():
-                    newObj = eval(obj['__class__'])(**obj)
-                    FileStorage.__objects[key] = newObj
+            if os.path.exists(FileStorage.__file_path):
+                with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    for key, obj in data.items():
+                        newObj = eval(obj['__class__'])(**obj)
+                        FileStorage.__objects[key] = newObj
         except FileNotFoundError:
             pass
