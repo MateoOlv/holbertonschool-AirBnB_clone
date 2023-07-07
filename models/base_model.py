9745d787caab8298a1d892056a7141c74c50ae77
MateoOlv <6304@holbertonstudents.com>
2023-07-07 07:43:36 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,6 @@ import models
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Init Value"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        models.storage.new(self)
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -24,6 +20,11 @@ class BaseModel:
                         value = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """Str return"""
