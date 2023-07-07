9de2304a2f2c70abebd632195157d196b5be919e
MateoOlv <6304@holbertonstudents.com>
2023-07-07 07:45:43 -0700

    changed if

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,11 @@ class BaseModel:
         """Init Value"""
         if kwargs is not None:
             for key, value in kwargs.items():
+
+                if key == "update_at" or key == "created_at":
+                    value = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
-                    if key == "update_at" or key == "created_at":
-                        value = datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
